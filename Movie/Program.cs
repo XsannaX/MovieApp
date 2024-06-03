62aa58bc275f0101ef04cc3dc4151e8b7ecbecf9
XsannaX <alexa060402@gmail.com>
2024-06-03 18:45:58 +0200

    Reapply "Done"
    This reverts commit bdceb1a6131e97e224b57ce9a4793ed1ae2843cd.

--- Movie/Program.cs
+++ Movie/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Authentication.Certificate;
+﻿using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
